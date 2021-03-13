f1ee428f5c4af18d4f02cc58023cf0e317d483eb
MariamZiade <mariam.ziade@gmail.com>
2021-03-14 01:43:51 +0200

    Step 4 Done

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,3 @@
+var name=prompt("Please enter your name: ","Your name here");
+var surname=prompt("Please enter your Surname ","Your Surname here");
+var city=prompt("Please enter your City: ","Your City here");
