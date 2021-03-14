c82b05faf3bf6211a46080a538663c03c608e465
MariamZiade <mariam.ziade@gmail.com>
2021-03-14 11:40:13 +0200

    Step 7 Done

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,4 @@
+var shoe=prompt("Enter you shoe size please" , "Shoe size");
+var birth=prompt("Enter your birth year please" , "Birthyear");
+var result=(shoe)*(2)+(5)*(50)-(birth)+(1766);
+alert("Result: "+ result);
