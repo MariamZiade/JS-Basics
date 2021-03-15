c3bcb0ce894be7507f4975487423b86fe46bb598
MariamZiade <mariam.ziade@gmail.com>
2021-03-15 14:54:45 +0200

    Step 1 Done

--- javascript-basics-02/step 1/Step_1.js
+++ javascript-basics-02/step 1/Step_1.js
@@ -1,8 +1,8 @@
 function changePic1() {
     document.querySelector("#image1")
-    image1.src="images/image1_2.jpg"
+    image1.src="images/image1.jpg"
 }
 function changePic2(){
     document.querySelector("#image2")
-    image1.src="images/image1.jpg"
+    image1.src="images/image1_2.jpg"
 }
